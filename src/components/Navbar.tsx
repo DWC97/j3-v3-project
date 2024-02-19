626edbc32d63b9341ef853fda836080a6e0972c6
DWC97 <dwcarton1@gmail.com>
2024-02-19 16:36:23 +0000

    navbar shrink and reappear on scroll from top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,13 @@
 
 import gsap from "gsap"; 
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Navbar(){
 
     let navbar = useRef(null)
+    const [isShrunk, setIsShrunk] = useState(false) // set height of navbar based on scroll distance from top
+    const [isFixed, setIsFixed] = useState(false) // set fixed position of navbar based on scroll distance from top
 
     useGSAP(() => {
         gsap.fromTo(
@@ -26,10 +28,33 @@ export default function Navbar(){
         )
     })
 
+
+    // managing navbar height and visibility based on scroll position from top of home page
+    function scrollFunction(){
+        if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+            setIsFixed(true)
+            setIsShrunk(true)
+        }
+        else {
+            setIsFixed(false)
+            setIsShrunk(false)
+        }
+    }
+
+    function loadEvents(){
+        if (typeof window !== 'undefined') {
+                window.onscroll = function() {
+                    scrollFunction()
+                }
+            } 
+        };
+
+    loadEvents()
+
     return (
-        <div className="absolute left-0 top-0 z-[1000] w-screen h-20" ref={navbar}>
+        <div className={`left-0 top-0 z-[1000] w-screen ${isShrunk ? "h-16 bg-black bg-opacity-50" : "h-20"} ${isFixed ? "fixed ease-in-out duration-300" : "absolute"}`} ref={navbar}>
             <div className="relative">
-                <img src="/logo.png" className="absolute left-0 right-0 m-auto my-auto w-20 mt-2"/>
+                <img src="/logo.png" className={`absolute left-0 right-0 m-auto my-auto ${isShrunk ? "w-16" : "w-20"} mt-2 ease-in-out duration-300`}/>
             </div>
             <div className="flex flex-row w-screen h-full ">
                 <div className="w-1/2 h-full flex justify-end text-white">
@@ -40,7 +65,7 @@ export default function Navbar(){
                 <div className="w-1/2 right-1/2 h-full flex justify-between text-white">
                     <span className="mt-1 h-full flex items-center justify-center w-44 ml-24 border-opacity-0 hover:border-opacity-100 border-b-2 border-custom-pink ease-in-out duration-300">STORE</span>
                     <div className='flex justify-center items-center w-44'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} viewBox="0 0 24 24"><path fill="white" d="M7.308 21.115q-.633 0-1.067-.433q-.433-.434-.433-1.067q0-.632.433-1.066q.434-.434 1.067-.434q.632 0 1.066.434q.434.434.434 1.066q0 .633-.434 1.067q-.434.433-1.066.433m9.384 0q-.632 0-1.066-.433q-.434-.434-.434-1.067q0-.632.434-1.066q.434-.434 1.066-.434q.633 0 1.067.434q.433.434.433 1.066q0 .633-.433 1.067q-.434.433-1.067.433M5.881 5.5l2.669 5.615h6.635q.173 0 .307-.086q.135-.087.231-.24l2.615-4.75q.116-.212.02-.376q-.096-.163-.327-.163zm-.489-1h13.02q.651 0 .98.532q.33.531.035 1.095l-2.858 5.208q-.217.365-.564.573q-.347.207-.763.207H8.1l-1.215 2.231q-.154.23-.01.5t.433.27h10.884v1H7.308q-.875 0-1.306-.738q-.43-.738-.021-1.482l1.504-2.68L3.808 3.5H2v-1h2.442zm3.158 6.615h7z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={isShrunk ? 32 : 40} height={isShrunk ? 32 : 40} viewBox="0 0 24 24"><path fill="white" d="M7.308 21.115q-.633 0-1.067-.433q-.433-.434-.433-1.067q0-.632.433-1.066q.434-.434 1.067-.434q.632 0 1.066.434q.434.434.434 1.066q0 .633-.434 1.067q-.434.433-1.066.433m9.384 0q-.632 0-1.066-.433q-.434-.434-.434-1.067q0-.632.434-1.066q.434-.434 1.066-.434q.633 0 1.067.434q.433.434.433 1.066q0 .633-.433 1.067q-.434.433-1.067.433M5.881 5.5l2.669 5.615h6.635q.173 0 .307-.086q.135-.087.231-.24l2.615-4.75q.116-.212.02-.376q-.096-.163-.327-.163zm-.489-1h13.02q.651 0 .98.532q.33.531.035 1.095l-2.858 5.208q-.217.365-.564.573q-.347.207-.763.207H8.1l-1.215 2.231q-.154.23-.01.5t.433.27h10.884v1H7.308q-.875 0-1.306-.738q-.43-.738-.021-1.482l1.504-2.68L3.808 3.5H2v-1h2.442zm3.158 6.615h7z"></path></svg>
                     </div>
                 </div>
             </div>
