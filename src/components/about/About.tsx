3be89ca569cf6a6f0f222d95aca3b437eee9eb9b
DWC97 <dwcarton1@gmail.com>
2024-02-21 12:24:19 +0000

    top section responsive for all devices

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,14 +2,14 @@ export default function About(){
     return (
         <div className="overflow-hidden relative bg-black">
             {/* <div className="absolute bottom-0 left-0 w-full h-[250px] bg-gradient-to-b from-black to-custom-blue opacity-60"></div> */}
-            <div className="w-full lg:h-[624px] flex flex-row px-16 ">
+            <div className="w-full lg:h-[624px] flex flex-row px-8 md:px-16 ">
                 <div className="flex flex-col lg:flex-row w-screen items-center justify-center lg:max-h-[402px] mt-48 border-2 border-red-500 mb-16 lg:mb-0">
-                    <div className="flex flex-col h-[400px] mb-16 lg:mb-0 max-w-[850px] lg:min-h-[402px] justify-between">
-                        <h2 className="bg-gradient-to-r from-custom-blue  to-custom-yellow inline-block text-transparent bg-clip-text text-[44px] font-semibold tracking-wide min-[1366px]:text-[48px] 2xl:text-[56px]">SMALL GROUP PARTY TOURS</h2>
-                        <p className="text-white text-[16px] xl:text-[18px] ">
+                    <div className="flex flex-col h-[400px] mb-12 md:mb-16 lg:mb-0 max-w-[850px] lg:min-h-[402px] justify-between">
+                        <h2 className="bg-gradient-to-r from-custom-blue  to-custom-yellow inline-block text-transparent bg-clip-text text-[28px] md:text-[44px] lg:text-[36px] xl:text-[44px] font-semibold tracking-wide min-[1366px]:text-[48px] 2xl:text-[56px] max-[480px]:text-[24px] max-[416px]:text-[20px] lg:text-left text-center">SMALL GROUP PARTY TOURS</h2>
+                        <p className="text-white text-[16px] xl:text-[18px] max-[480px]:text-[14px]">
                             Ahoy there! If you’re looking to experience the best nightlife & culture South-East Asia has to offer with a rowdy group of travellers and tick-off bucket-list activities along the way, JR has you covered. We make logistics stress-free so you can focus on having a good time.
                         </p>
-                        <p className="text-white text-[16px] xl:text-[18px]  z-10">    
+                        <p className="text-white text-[16px] xl:text-[18px]  z-10 max-[480px]:text-[14px]">    
                             Be warned, we do things a little differently over at JR: 
                             <br/>&nbsp;❌ &nbsp;No couples
                             <br/>&nbsp;&nbsp;🤘&nbsp;&nbsp; 18– 30 only
@@ -17,7 +17,7 @@ export default function About(){
                         </p>
                         <div className="h-10 w-full bg-gradient-to-r from-custom-orange to-custom-pink p-[2px] rounded-md cursor-pointer mt-8 z-10">
                             <div className="h-full w-full bg-black flex items-center rounded-md hover:bg-opacity-0 duration-300 ease-in-out">
-                                <div className="bg-gradient-to-r from-custom-orange  to-custom-pink flex justify-center items-center text-transparent bg-clip-text h-full w-full font-semibold hover:text-white ease-in-out duration-300 xl:text-[18px]">
+                                <div className="bg-gradient-to-r from-custom-orange  to-custom-pink flex justify-center items-center text-transparent bg-clip-text h-full w-full font-semibold hover:text-white ease-in-out duration-300 xl:text-[18px] max-[480px]:text-[14px]">
                                     SEE FULL TOUR SELECTION
                                 </div>
                             </div>
