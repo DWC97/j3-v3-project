89c3233ee6bdf7c8a8424ba93bb9602a05acef20
DWC97 <dwcarton1@gmail.com>
2024-02-21 16:18:13 +0000

    gradient overlay

--- src/components/tour-selection/TourSelection.tsx
+++ src/components/tour-selection/TourSelection.tsx
@@ -2,6 +2,7 @@ export default function TourSelection(){
     return (
         <div className="w-full relative h-[1080px] ">
             <div className="bg-[url('/tour-selection/beach.jpg')] w-full h-full bg-center bg-no-repeat bg-cover bg-fixed relative" />
+            <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-b from-custom-blue to-custom-yellow opacity-80" />
         </div>
     )
 }
