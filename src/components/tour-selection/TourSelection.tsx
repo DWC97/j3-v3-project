34bea506f2f86a9217a5ef3aacccfe953df00a70
DWC97 <dwcarton1@gmail.com>
2024-02-26 11:42:30 +0000

    migrated swiper

--- src/components/tour-selection/TourSelection.tsx
+++ src/components/tour-selection/TourSelection.tsx
@@ -1,3 +1,18 @@
+"use client"
+
+import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-coverflow';
+import "./SliderStyles.css"
+import toursData from "@/data/tours.json"
+
 export default function TourSelection(){
     return (
         <div className="w-full relative h-[1080px] flex justify-center items-center">
@@ -7,6 +22,56 @@ export default function TourSelection(){
                 <h2 className="text-[44px] text-white font-semibold tracking-wide">
                     CHECK OUT OUR <span className="text-custom-yellow">UPCOMING TRIPS</span>!
                 </h2>
+                <div className='w-full flex justify-center items-center'>
+                    <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    loop={true}
+                    speed={1000}
+                    slidesPerView={3}
+                    coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 3.5,
+                    }}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                    }}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className="swiper_container"
+                    >
+                        {toursData.tours.map(tour => {
+                            return (
+                                <SwiperSlide>
+                                    <div className='card-container'>
+                                        <img src={tour.imageSrc}/>
+                                        <div className='card-content'>
+                                            <span className='region'>{tour.region}</span>
+                                            <span className='title'>{tour.title}</span>
+                                            <div className="hidden-content flex flex-col">
+                                                <div className='flex flex-row justify-between items-center pb-3'>
+                                                    <p className='italic'>{tour.duration} days</p>
+                                                    <p className='font-semibold text-xl -mr-4'>£{tour.price}</p>
+                                                </div>
+                                            </div>
+                                        </div>    
+                                    </div> 
+                                </SwiperSlide>
+                            )
+                        })}
+                        <div className="slider-controler">
+                            <div className="swiper-button-prev slider-arrow">
+                            </div>
+                            <div className="swiper-button-next slider-arrow">
+                            </div>
+                            <div className="swiper-pagination"></div>
+                        </div>
+                    </Swiper>
+                </div>
                 <p className="text-[20px] text-gray-100">
                     Each tour includes transport, accommodation and activities.
                 </p>
