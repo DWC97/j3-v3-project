348734c0fe0f2581fec771b3b324465a9e043941
DWC97 <dwcarton1@gmail.com>
2024-02-28 16:29:04 +0000

    tour data cleanup

--- src/app/[tourId]/page.tsx
+++ src/app/[tourId]/page.tsx
@@ -1,15 +1,18 @@
+"use client"
+
 import toursData from "@/data/tours.json"
 
 export default function tourDetails({ params }: { params: { tourId: string }}){
 
-    // const tour = toursData.tours.find(tour => {
-    //     return tour.region === params.tourId
-    // })
-    // console.log(tour)
+    const tour = toursData.tours.find(tour => {
+        return tour.region === params.tourId
+    })
+    console.log(toursData)
+    console.log(tour)
 
     return (
         <div>
-            tour page for {params.tourId}
+            tour page for {tour?.region}
         </div>
     )
 }
