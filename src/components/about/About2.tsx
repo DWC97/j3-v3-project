efe2ba80df89cc0f49c67adcc6914203a8746fab
DWC97 <dwcarton1@gmail.com>
2024-03-11 10:59:58 +0000

    Markup started for new about section

---
+++ src/components/about/About2.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect } from 'react';
+import './AboutStyles.css'
+import Image from 'next/image';
+import Lenis from '@studio-freight/lenis'
+
+const images = [
+    "/about/pirate-polaroid.png",
+    "/about/tubing-polaroid.png",
+    "/about/waterpark-polaroid.png",
+    "/about/muaythai-polaroid.png",
+    "/about/pirate-polaroid.png",
+    "/about/tubing-polaroid.png",
+    "/about/waterpark-polaroid.png",
+    "/about/muaythai-polaroid.png"
+]
+
+export function About2(){
+    return (
+        <div>
+
+        </div>
+    )
+}
