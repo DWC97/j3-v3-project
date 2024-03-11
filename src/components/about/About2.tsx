02aa76e8499e9e5c1fa0f9d97dbab1dcdbe56c7b
DWC97 <dwcarton1@gmail.com>
2024-03-11 13:45:21 +0000

    column speed somewhat better

--- src/components/about/About2.tsx
+++ src/components/about/About2.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useTransform, useScroll, motion } from 'framer-motion';
 import './AboutStyles.css'
 import Image from 'next/image';
+import Lenis from '@studio-freight/lenis'
 
 const images = [
     "/about/pirate-polaroid.png",
@@ -34,8 +35,11 @@ export function About2(){
     })
 
     const { height } = dimension;
-    const y = useTransform(scrollYProgress, [0, 1], [0, height * 2])
-    const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3])
+    const y = useTransform(scrollYProgress, [0, 1], [0, height * 1.5])
+    const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
+    const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25])
+
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
 
     useEffect( () => {
 
@@ -54,14 +58,17 @@ export function About2(){
 
     return (
         <main className="main">
-     
+            <div className='h-screen w-full bg-green-200' />
                 <div className="gallery" ref={gallery}>
                     <div className="galleryWrapper">
                         <Column images={[images[0], images[1], images[2], images[3]]} y={y}/>
                         <Column images={[images[4], images[5], images[6], images[7]]} y={y2}/>
+                        {/* <Column images={[images[0], images[1], images[2], images[3]]} y={y3}/>
+                        <Column images={[images[4], images[5], images[6], images[7]]} y={y4}/> */}
+                        
                     </div>
                 </div>
-     
+            <div className='h-screen w-full bg-green-200' />
         </main>
     )
 }
