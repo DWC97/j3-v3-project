806ee4aaccefaf60ceea904bf9327333e5911f22
DWC97 <dwcarton1@gmail.com>
2024-03-13 12:32:28 +0000

    active about nav link + color palette change

--- src/components/about/About2.tsx
+++ src/components/about/About2.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useContext } from 'react';
+import { ActiveSectionContext } from "@/context/ActiveSectionContext"
+import useDetectSection from "@/hooks/useDetectSection"
 import { useTransform, useScroll, motion } from 'framer-motion';
 import './AboutStyles.css'
 import Image from 'next/image';
@@ -72,6 +74,19 @@ const imageData = [
 
 export function About2(){
 
+    let { setActiveSection } = useContext(ActiveSectionContext)
+    const aboutRef = useRef(null)
+    const [isInView] = useDetectSection(aboutRef)
+
+    useEffect(() => {
+        if (isInView){
+            setActiveSection("about")
+        }
+        else {
+            setActiveSection("")
+        }
+    }, [isInView])
+
     const gallery = useRef(null);
     const [dimension, setDimension] = useState({width:0, height:0});
 
@@ -102,11 +117,11 @@ export function About2(){
       }, [])
 
     return (
-        <div className="flex flex-row bg-black h-[1080px] relative">
-            <div className='absolute bottom-0 left-0 w-full h-[500px] bg-gradient-to-b from-transparent to-custom-blue opacity-20' />
+        <div className="flex flex-row bg-black h-[1080px] relative" id="about" ref={aboutRef}>
+            <div className='absolute bottom-0 left-0 w-full h-[500px] bg-gradient-to-b from-transparent to-custom-blue opacity-25' />
             <div className='absolute bottom-0 left-0 w-full h-[500px] bg-gradient-to-br from-transparent to-custom-blue opacity-20' />
-            <div className='flex flex-col h-full w-3/5 justify-center px-[8%]'>
-                <h3 className='text-custom-pink text-[20px] font-semibold mb-2'>JR Season 1</h3>
+            <div className='flex flex-col h-full w-3/5 justify-center px-[8%] -pr-10'>
+                <h3 className='text-custom-blue text-[20px] font-semibold mb-2'>JR Season 1</h3>
                 <h2 className='text-white font-bold text-[40px] leading-snug mb-4'>Small group party tours starting in 2025</h2>
                 <p className='text-white text-[16px] leading-relaxed mb-2'>Ahoy there! If you’re looking to experience the best nightlife & culture South-East Asia has to offer with a rowdy group of travellers and tick-off bucket-list activities along the way, JR has you covered. We make logistics stress-free so you can focus on having a good time. Be warned, we do things a little differently over at JR:</p>
                 <div className='flex flex-row items-center'>
@@ -122,7 +137,7 @@ export function About2(){
                     <span className='font-semibold text-white text-[16px]'>Be prepared for LOTS of drinking...</span>
                 </div>
                 <div className='flex flex-row items-center'>
-                    <button className='font-semibold  text-white w-1/2 py-2 rounded-md bg-gradient-to-r from-custom-orange to-custom-pink text-[18px] mr-16'>Learn more</button>
+                    <button className='font-semibold  text-white w-1/2 py-2 rounded-md bg-gradient-to-r from-custom-blue to-custom-yellow text-[18px] mr-16'>Learn more</button>
                     <div className='flex flex-row items-center justify-center'>
                         <button className='text-white text-[18px]'>Get in touch</button>
                         <svg xmlns="http://www.w3.org/2000/svg" className='pl-3' width={40} height={32} viewBox="0 0 16 16"><path fill="white" fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path></svg>
@@ -131,7 +146,7 @@ export function About2(){
                 </div>
                 
             </div>
-            <div className="gallery" ref={gallery}>
+            <div className="gallery pl-10" ref={gallery}>
                 <div className="galleryWrapper">
                     <Column imagesData={[imageData[0], imageData[1], imageData[2], imageData[3], imageData[4], imageData[5]]} y={y}/>
                     <Column imagesData={[imageData[6], imageData[7], imageData[8], imageData[9], imageData[10], imageData[11]]} y={y2}/>                 
