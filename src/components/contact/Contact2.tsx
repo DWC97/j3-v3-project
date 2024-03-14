f3cbec720f2b61da28e334f1471d3626aeec6e62
DWC97 <dwcarton1@gmail.com>
2024-03-14 13:27:12 +0000

    cta for contact section

--- src/components/contact/Contact2.tsx
+++ src/components/contact/Contact2.tsx
@@ -1,5 +1,6 @@
 import { ActiveSectionContext } from "@/context/ActiveSectionContext"
 import useDetectSection from "@/hooks/useDetectSection"
+import Link from "next/link"
 import { useState, useEffect, useRef, useContext } from "react"
 
 export default function Contact2(){
@@ -17,7 +18,14 @@ export default function Contact2(){
 
     return (
         <div className="bg-black w-full h-screen contact flex flex-row items-center justify-around pt-20" id="contact" ref={contactRef}>
-            <img src="/contact/Jolly_Roger.jpg" className="w-[400px] -mr-20"/>
+            <div className="-mr-20 flex flex-col items-center justify-between h-[420px]">
+                <img src="/contact/Jolly_Roger.jpg" className="w-[360px] "/>
+                <Link href={"/store"} className='flex flex-row items-center justify-center'>
+                    <button className='text-white text-[18px]'>Get our merch</button>
+                    <svg xmlns="http://www.w3.org/2000/svg" className='pl-3' width={40} height={32} viewBox="0 0 16 16"><path fill="white" fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path></svg>
+                </Link>
+            </div>
+           
             <div className="flex flex-col justify-between w-[500px] md:w-[600px] h-[420px] bg-black bg-opacity-50 rounded-3xl z-30 -ml-20">
                 
                 <h2 className="font-semibold text-[44px] text-white">RESERVE A SPOT</h2>
