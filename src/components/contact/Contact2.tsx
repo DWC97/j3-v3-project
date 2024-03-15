5b387ce05a0c0c384dca83a985691522984d34e3
DWC97 <dwcarton1@gmail.com>
2024-03-15 13:30:57 +0000

    bugs fixed

--- src/components/contact/Contact2.tsx
+++ src/components/contact/Contact2.tsx
@@ -10,11 +10,10 @@ export default function Contact2(){
     let { setActiveSection } = useContext(ActiveSectionContext)
     const contactRef = useRef(null)
     const [isInView] = useDetectSection(contactRef)
-    const [toggle] = useBodyLockScroll() // toggle scroll lock
 
     const [formData, setFormData] = useState({
-        email: "",
-        password: ""
+        name: "",
+        email: ""
     });
 
     const [errors, setErrors] = useState({});
@@ -64,19 +63,26 @@ export default function Contact2(){
             <div className="flex flex-col justify-between w-[500px] md:w-[600px] h-[500px] bg-black bg-opacity-50 rounded-3xl z-30 -ml-20">
                 
                 <h2 className="font-semibold text-[44px] text-white">RESERVE A SPOT</h2>
-                <form action="" className="flex flex-col justify-between h-[300px] relative">
-                    <input type="text" placeholder="Full name*" className="w-[240px] placeholder-gray-300 text-white bg-transparent border-b border-white p-2 focus:outline-none"
+                <form action="" className="flex flex-col justify-between h-[300px] relative" autoComplete="off">
+                    <input type="text" name="name" placeholder="" className="w-[240px] placeholder-gray-300 text-white border-b border-white px-2 pb-2 outline-none !bg-black"
+                    value={formData.name}
+                    onChange={handleInputChange}
                     /> 
-                    <div className="absolute text-[red] -top-6">Name is required</div>  
-                    <div className="absolute top-24 left-56" title="Please include an '@' in the email address">
+                    <div className="absolute text-white -top-8">Full name</div>  
+                    <div className="absolute top-1 left-56" title="Please include an '@' in the email address">
                         <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24"><path fill="red" d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.712T12 15q-.425 0-.712.288T11 16q0 .425.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"></path></svg>
                     </div>
-                    <input type="text" placeholder="Email*" className="w-[240px] placeholder-gray-300 text-white bg-transparent border-b border-white p-2 focus:outline-none"
+                    <div className="absolute top-[5.75rem] left-56" title="Please include an '@' in the email address">
+                        <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24"><path fill="red" d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.712T12 15q-.425 0-.712.288T11 16q0 .425.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"></path></svg>
+                    </div>
+                    <input type="text" name="email" placeholder="" className="w-[240px] placeholder-gray-300 text-white border-b border-white pl-2 pr-6 pb-2 mt-2 outline-none bg-black"
+                    value={formData.email}
+                    onChange={handleInputChange}
                     />   
-                    <div className="absolute text-[red] top-16">Enter a valid email</div>  
+                    <div className="absolute text-white top-[3.75rem]">Email</div>  
                     <div className="flex flex-row w-full justify-between items-center">
                         <div className="w-[240px]">
-                            <select id="country" name="country" className="block w-full p-2 text-gray-300 bg-black border-white border-b outline-none">
+                            <select id="country" name="country" className="block w-full p-2 text-white bg-black border-white border-b outline-none">
                                 <option>Northern Thailand</option>
                                 <option>Southern Thailand</option>
                                 <option>Vietnam</option>
@@ -84,8 +90,7 @@ export default function Contact2(){
                                 <option>Philippines</option>
                             </select>
                         </div>
-                        {/* <input type="text" placeholder="Destination*" className="w-[240px] placeholder-gray-300 text-white bg-transparent border-b border-white p-2 focus:outline-none"/>   */}
-                        <input type="number" min={1} max={4} defaultValue={1} className="w-[240px] placeholder-gray-300 text-white bg-transparent border-b border-white p-2 focus:outline-none"/>  
+                        <input type="number" min={1} max={4} defaultValue={1} className="w-[240px] text-white border-b border-white p-2 outline-none bg-black"/>  
                     </div>
                     <div className="absolute text-white top-[9.5rem]">Destination</div>  
                     <div className="absolute text-white right-[140px] top-[9.5rem]">No. of people</div>  
