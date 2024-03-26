41008a0bc13727e2faf56cf19cd91c6dca9dd296
DWC97 <dwcarton1@gmail.com>
2024-03-26 16:50:27 +0000

    functions for ShoppingCartContext

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,9 +1,6 @@
 "use client"
 
-import { useState } from "react"
 import { ActiveSectionProvider } from "./ActiveSectionContext"
-import SmoothScrolling from "./SmoothScrolling"
-
 
 export function Providers({ children }){
     return (
