f22588efe76566cab3157de6a641e219289d1eda
DWC97 <dwcarton1@gmail.com>
2024-03-27 15:32:03 +0000

    cart item markup

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,25 @@
+import storeItemsData from "@/data/storeItems.json"
+
+export default function CartItem({ id, quantity, size }){
+
+    const item = storeItemsData.items.find(item => item.id === id)
+
+    return (
+        <div className="flex flex-row gap-6">
+            <img src={item?.gallery[0]} className="w-24 h-24 object-cover border border-gray-300 rounded-md"/>
+            <div className="flex flex-col justify-between w-full">
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-col">
+                        <span className="font-semibold">{item?.name}</span>
+                        <span className="text-gray-500 text-[14px]">{size}</span>
+                    </div>
+                    <span className="font-semibold">£{item?.price}.00</span>
+                </div>
+                <div className="flex flex-row justify-between items-center">
+                    <span className="text-gray-500 text-[14px]">Qty {quantity}</span>
+                    <span className="text-custom-pink text-[14px]">Remove</span>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,28 @@
-export default function ShoppingCart(){
-    return (
-        <div className="z-[10000] w-full h-screen bg-black bg-opacity-70 absolute top-0  left-0">
+"use client"
 
+import { ShoppingCartContext } from "@/context/ShoppingCartContext"
+import { useContext } from "react"
+import CartItem from "./CartItem"
+
+export default function ShoppingCart(){
+
+    const { cartItems } = useContext(ShoppingCartContext)
+
+    return (
+        <div className="z-[10000] w-full h-screen bg-black bg-opacity-70 absolute top-0 left-0 flex flex-row justify-end">
+            <div className="w-1/3 bg-white h-full border-y border-gray-100 flex flex-col">
+                <div className="w-full flex flex-row justify-between px-8 items-center my-10">
+                    <span className="text-[20px] font-medium">Shopping cart</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 16 16"><path fill="gray" d="M7.293 8L3.146 3.854a.5.5 0 1 1 .708-.708L8 7.293l4.146-4.147a.5.5 0 0 1 .708.708L8.707 8l4.147 4.146a.5.5 0 0 1-.708.708L8 8.707l-4.146 4.147a.5.5 0 0 1-.708-.708z"></path></svg>
+                </div>
+                <div className="px-8">
+                    {cartItems.map((item, i) => {
+                        return (
+                            <CartItem key={i} {...item}/>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
