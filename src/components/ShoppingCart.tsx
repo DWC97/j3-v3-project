e75e1a839868008539c44713e08fec075dae2904
DWC97 <dwcarton1@gmail.com>
2024-03-27 16:12:28 +0000

    cart subtotal section markup

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -9,9 +9,9 @@ export default function ShoppingCart(){
     const { cartItems } = useContext(ShoppingCartContext)
 
     return (
-        <div className="z-[10000] w-full h-screen bg-black bg-opacity-70 absolute top-0 left-0 flex flex-row justify-end">
-            <div className="w-1/3 bg-white h-full border-y border-gray-100 flex flex-col">
-                <div className="w-full flex flex-row justify-between px-8 items-center my-10">
+        <div className="z-[10000] w-full h-screen bg-black bg-opacity-70 absolute top-0 left-0 flex flex-row justify-end backdrop-blur-sm">
+            <div className="w-1/3 bg-white h-full border-y border-gray-100 flex flex-col relative">
+                <div className="w-full flex flex-row justify-between px-8 items-center mt-7 mb-2">
                     <span className="text-[20px] font-medium">Shopping cart</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 16 16"><path fill="gray" d="M7.293 8L3.146 3.854a.5.5 0 1 1 .708-.708L8 7.293l4.146-4.147a.5.5 0 0 1 .708.708L8.707 8l4.147 4.146a.5.5 0 0 1-.708.708L8 8.707l-4.146 4.147a.5.5 0 0 1-.708-.708z"></path></svg>
                 </div>
@@ -21,6 +21,20 @@ export default function ShoppingCart(){
                             <CartItem key={i} {...item}/>
                         )
                     })}
+                </div>
+                <div className="absolute flex flex-col justify-around border-t border-gray-300 bottom-0 left-0 h-48 w-full px-8">
+                    <div className="flex flex-row justify-between">
+                        <div className="flex flex-col">
+                            <span className="font-semibold">Subtotal</span>
+                            <span className="text-gray-500 text-[14px]">Shipping & taxes calculated at checkout.</span>
+                        </div>
+                        <span className="font-semibold">£262.00</span>
+                    </div>
+                    <button className="w-full py-3 rounded-md font-semibold bg-custom-pink text-white">Checkout</button>
+                    <div className="flex flex-row w-full justify-center gap-1 items-center">
+                        <span className="text-gray-500 text-[14px]">or <span className="text-custom-pink">Continue Shopping</span></span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className='' width={12} height={12} viewBox="0 0 16 16"><path fill="#ec2aa2" fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path></svg>
+                    </div>
                 </div>
             </div>
         </div>
