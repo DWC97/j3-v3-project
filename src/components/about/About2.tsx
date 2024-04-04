e7b7c5ae0fb777864ee835db38488e4ff2ef2353
DWC97 <dwcarton1@gmail.com>
2024-04-04 17:37:59 +0100

    about section responsiveness for mobile

--- src/components/about/About2.tsx
+++ src/components/about/About2.tsx
@@ -121,10 +121,10 @@ export function About2(){
         <div className="flex flex-row bg-black min-h-screen h-[1080px] relative" id="about" ref={aboutRef}>
             {/* <div className='z-0 absolute bottom-0 left-0 w-full h-[300px] bg-gradient-to-b from-transparent to-custom-blue opacity-20' />
             <div className='z-0 absolute bottom-0 left-0 w-full h-[300px] bg-gradient-to-br from-transparent to-custom-blue opacity-10' /> */}
-            <div className={`z-10 flex flex-col h-full lg:w-3/5 w-4/5 justify-center px-[7%] -pr-10 `}>
+            <div className={`z-10 flex flex-col h-full lg:w-3/5 sm:w-4/5 w-full justify-center px-10 sm:px-[7%] -pr-10 `}>
                 <Reveal>
                     <h3 className='text-custom-blue text-[20px] font-bold mb-2'>JR SEASON 1</h3>
-                    <h2 className='text-white font-bold text-[40px] leading-snug mb-6'>Small group party tours starting in 2025</h2>
+                    <h2 className='text-white font-bold md:text-[40px] text-[32px] leading-snug mb-6'>Small group party tours starting in 2025</h2>
                     <p className='text-white text-[16px] leading-relaxed mb-2'>Ahoy there! If you’re looking to experience the best nightlife & culture South-East Asia has to offer with a rowdy group of travellers and tick-off bucket-list activities along the way, JR has you covered. We make logistics stress-free so you can focus on having a good time. Be warned, we do things a little differently over at JR:</p>
                     <div className='flex flex-row items-center'>
                         <span className='text-[20px] mr-4'>❌</span>
@@ -138,8 +138,8 @@ export function About2(){
                         <span className='text-[20px] mr-4'>🍻</span>
                         <span className='font-medium text-white text-[16px]'>Be prepared for LOTS of drinking...</span>
                     </div>
-                    <div className='flex flex-row items-center gap-16'>   
-                        <Link href={"/#tours"} className='z-[1000] w-1/2 cursor-pointer font-semibold text-center text-white  py-2 rounded-md bg-gradient-to-r from-custom-blue to-custom-yellow text-[18px] hover:opacity-85 ease-in-out duration-300'
+                    <div className='flex flex-col sm:flex-row items-center gap-8 md:gap-16'>   
+                        <Link href={"/#tours"} className='z-[1000] w-full sm:w-1/2 cursor-pointer font-semibold text-center text-white  py-2 rounded-md bg-gradient-to-r from-custom-blue to-custom-yellow text-[18px] hover:opacity-85 ease-in-out duration-300'
                         >Learn more</Link>
                         <Link href={"/#contact"} className='flex flex-row items-center justify-center hover:opacity-85 ease-in-out duration-300'>
                             <button tabIndex={-1} className='text-white text-[18px]'>Get in touch</button>
@@ -149,7 +149,7 @@ export function About2(){
                 </Reveal>
                 
             </div>
-            <div className="gallery pl-10" ref={gallery}>
+            <div className="gallery pl-10 hidden sm:flex" ref={gallery}>
                 
                     {window.innerWidth > 1024 ? 
                     <div className="galleryWrapper">
