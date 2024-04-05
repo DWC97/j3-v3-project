e45e6daedc4fdcb1ba14cacfe3e74d88312c53c1
DWC97 <dwcarton1@gmail.com>
2024-04-05 11:43:27 +0100

    404 page and footer responsiveness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound(){
             <div className="z-10 -mt-20 flex flex-col items-center justify-center gap-2">
                 <span className="text-white font-medium">404</span>
                 <h1 className="text-white font-bold text-[40px] leading-snug">Page not found</h1>
-                <span className="text-gray-300">Sorry, we couldn’t find the page you’re looking for.</span>
+                <span className="text-gray-300 px-10 text-center">Sorry, we couldn’t find the page you’re looking for.</span>
                 <Link href={"/"} className="mt-8 flex flex-row items-center hover:opacity-85 ease-in-out duration-300 gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className='rotate-180' width={20} height={20} viewBox="0 0 16 16"><path fill="white" fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path></svg>
                     <span className="text-gray-200 text-[14px] font-semibold">BACK TO HOME</span>
