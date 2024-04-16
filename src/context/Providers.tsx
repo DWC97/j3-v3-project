f1254fbbf27e564af572e3be0b682fe04d15aae5
DWC97 <dwcarton1@gmail.com>
2024-04-16 17:32:11 +0100

    useMounted hook in Providers component

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -3,12 +3,19 @@
 import { ReactNode } from "react"
 import { ActiveSectionProvider } from "./ActiveSectionContext"
 import { ShoppingCartProvider } from "./ShoppingCartContext"
+import { useMounted } from "@/hooks/useMounted"
 
 type ProviderProps = {
     children: ReactNode
 }
 
 export function Providers({ children }: ProviderProps){
+    const mounted = useMounted()
+    if (!mounted){
+        return (
+            <div className="bg-black min-h-screen h-[1080px]" />
+        )
+    }
     return (
         <ActiveSectionProvider>
             <ShoppingCartProvider>
