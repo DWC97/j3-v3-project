dd82499a9af19968060248258b0fdf0ac4348d33
DWC97 <dwcarton1@gmail.com>
2024-04-16 15:13:40 +0100

    useMounted hook added for conditional rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ import Hero from "@/components/home/Hero";
 import TourSelection from "@/components/tour-selection/TourSelection";
 import About2 from "@/components/about/About2";
 import Contact2 from "@/components/contact/Contact2";
+import { useMounted } from "@/hooks/useMounted";
+
 
 export default function Home() {
 
+  const mounted = useMounted()
+  
   return (
-      <div className="">
+      <>
         <Hero/>
+        {mounted ? 
         <About2 />
+        :
+        <div className="bg-black min-h-screen h-[1080px]"/>
+        }
         <TourSelection />
         <Contact2 />
-      </div>
+      </>
   );
 }
