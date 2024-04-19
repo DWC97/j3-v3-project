e3e46cc9a3e0e2af6f23aa19cf37d167a13c8a9d
DWC97 <dwcarton1@gmail.com>
2024-04-19 18:01:00 +0100

    Hero layers repositioned + her mobile component removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export default function Footer(){
                         <svg xmlns="http://www.w3.org/2000/svg" className="z-30" width={40} height={40} viewBox="0 0 24 24"><path fill="white" d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48"></path></svg>
                     </div>  
                 </a>
-                <div className='flex sm:hidden w-[28px] h-[28px] lg:w-[40px] lg:h-[40px] justify-center items-center'
+                <div className='flex sm:hidden w-[28px] h-[28px] lg:w-[40px] lg:h-[40px] justify-center items-center cursor-pointer'
                 onClick={() => {
                     if (typeof window !== 'undefined'){
                         window.scrollTo(0,0);
@@ -32,7 +32,7 @@ export default function Footer(){
                 </div>  
             </div>
             <p className="relative lg:absolute lg:left-0 lg:right-0 text-center text-[12px] text-gray-300">© 2024 JOLLY ROGER TOURS LTD. ALL RIGHTS RESERVED.</p>
-            <div  className='invisible sm:visible absolute right-10 lg:relative flex justify-center items-center w-[40px] h-[40px] z-40 hover:opacity-85 ease-in-out duration-300'
+            <div  className='invisible sm:visible absolute right-10 lg:relative flex justify-center items-center w-[40px] h-[40px] z-40 hover:opacity-85 ease-in-out duration-300 cursor-pointer'
             onClick={() => {
                 if (typeof window !== 'undefined'){
                     window.scrollTo(0,0);
